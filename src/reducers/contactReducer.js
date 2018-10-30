e34575f61be5420188338cd8edb64a6ae96f7e85
adam <atomicpenguines@gmail.com>
2018-10-30 21:10:59 +0000

    added ability to delete and add contacts using redux

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,4 +1,4 @@
-import { GET_CONTACTS } from "../actions/types";
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from "../actions/types";
 
 const initialState = {
   contacts: [
@@ -28,6 +28,19 @@ export default function(state = initialState, action) {
     case GET_CONTACTS:
       return {
         ...state
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    case ADD_CONTACT:
+      return {
+        ...state,
+        // add the new contact from the payload to the current contacts
+        contacts: [action.payload, ...state.contacts]
       };
     default:
       return state;
